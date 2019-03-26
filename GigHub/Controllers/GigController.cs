292c144d5eec015f5c2f4911caefd28bea637e73
Brett Hartman <brehardev@gmail.com>
2019-03-26 06:24:05 -0700

    Create GigController and ViewModel; create "Add a Gig" form on view at Create action

---
+++ GigHub/Controllers/GigController.cs
@@ -0,0 +1,28 @@
+﻿using GigHub.Models;
+using GigHub.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GigController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Gig
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
